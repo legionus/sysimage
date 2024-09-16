472e026bd58476214a31e1d1b534247205a4df1e
Alexey Gladkov <legion@altlinux.org>
2024-09-16 13:47:52 +0200

    gentoo: Add wrapper to run and comment some steps
    Signed-off-by: Alexey Gladkov <legion@altlinux.org>

--- vendor/gentoo/baseimage.sh
+++ vendor/gentoo/baseimage.sh
@@ -10,6 +10,7 @@ setup_binpkgs
 
 emerge-webrsync -q
 
+run_with_message "rebuild baseimage ..." \
 emerge \
 	${IMAGE_VAR_CACHE_BINPKGS:+--usepkg=y --buildpkg=y --binpkg-respect-use=y --rebuilt-binaries=y} \
 	--quiet --ask=n --emptytree --update --newuse \
@@ -17,6 +18,7 @@ emerge \
 
 # https://wiki.gentoo.org/wiki/Binary_package_guide
 [ -z "${IMAGE_VAR_REMOVE_BDEPS-}" ] ||
+	run_with_message "remove build deps ..." \
 	emerge --quiet --ask=n --depclean --with-bdeps=n
 
 restore_binpkgs
